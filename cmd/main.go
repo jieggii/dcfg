1d52d80ff5f84135c6e3ed6f6277b5dc5b0afc6e
jieggii <jieggii@pm.me>
2022-08-07 22:22:59 +0300

    Some small changes

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,42 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"github.com/jieggii/dcfg/cmd/config"
 	"github.com/urfave/cli/v2"
 	"os"
 )
 
-func logCommandUsageError(err error, command *cli.Command) {
+func usageErrorHandler(context *cli.Context, err error, isSubCommand bool) error {
+	// todo: add command name (init/update if context command is init/update)
 	fmt.Printf("Error: %v.\n", err)
-	fmt.Printf("Usage: %v. Type `dcfg %v --help` for more information.\n", command.UsageText, command.Name)
+	fmt.Println("Type `dcfg --help` for more information.")
+	os.Exit(1)
+	return nil
+}
+
+func logCommandUsageError(context *cli.Context, err string) {
+	var commandName string
+	var usageText string
+	if context.Command.Name != "" {
+		usageText = context.Command.UsageText
+		commandName = "dcfg " + context.Command.Name
+	} else {
+		usageText = context.App.UsageText
+		commandName = context.App.Name
+	}
+	fmt.Printf("Error: %v.\n", err)
+	fmt.Printf("Usage: %v.\n", usageText)
+	fmt.Printf("Type `%v --help` for more information.\n", commandName)
 }
 
 func main() {
 	app := &cli.App{
 		Name:        "dcfg",
 		Usage:       "distribute config",
-		UsageText:   "dcfg [command] [flags]",
+		UsageText:   "dcfg [flags] [command] [command flags]",
 		Version:     "0.1.0",
-		Description: "bruh",
+		Description: "Description...",
 		Authors: []*cli.Author{
 			{
 				Name:  "jieggii",
@@ -36,7 +53,7 @@ func main() {
 				Action: func(context *cli.Context) error {
 					cfgFilename := "dcfg.conf"
 					if context.Args().Len() != 0 {
-						logCommandUsageError(errors.New("too many arguments"), context.Command)
+						logCommandUsageError(context, "too many arguments")
 						os.Exit(1)
 					}
 					if err := config.CreateConfig(cfgFilename); err != nil {
@@ -46,15 +63,22 @@ func main() {
 					fmt.Printf("Created dcfg config file `%v` in the current working directory.\n", cfgFilename)
 					return nil
 				},
+				OnUsageError: usageErrorHandler,
 			},
 			{
-				Name:      "update",
-				Aliases:   []string{"u"},
-				Usage:     "update current working dir according to the dcfg config file",
-				UsageText: "dcfg update",
-				Description: "Updates current working directory according to the dcfg config file. " +
-					"Asks about deletions and overwritings if --yes flag is not used.",
-				Flags: []cli.Flag{},
+				Name:        "update",
+				Aliases:     []string{"u"},
+				Usage:       "update current working dir according to the dcfg config file",
+				UsageText:   "dcfg update",
+				Description: "Updates current working directory according to the dcfg config file.",
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:    "remove",
+						Aliases: []string{"r"},
+						Usage:   "remove directories and files that are not declared in config file as additions",
+						Value:   false,
+					},
+				},
 				Action: func(context *cli.Context) error {
 					cfgFilename := "dcfg.conf"
 					cfg, err := config.ReadConfig(cfgFilename)
@@ -71,18 +95,21 @@ func main() {
 					}
 					return nil
 				},
+				OnUsageError: usageErrorHandler,
 			},
 		},
 		HideHelpCommand: true,
 		Action: func(context *cli.Context) error {
-
+			args := context.Args()
+			if args.Len() == 0 {
+				logCommandUsageError(context, "no command provided")
+			} else {
+				logCommandUsageError(context, fmt.Sprintf("unknown command `%v`", args.First()))
+			}
+			os.Exit(1)
 			return nil
 		},
-		OnUsageError: func(context *cli.Context, err error, isSubCommand bool) error {
-			fmt.Printf("Error: %v.\n", err)
-			fmt.Println("Type `dcfg --help` for more information.")
-			return nil
-		},
+		OnUsageError: usageErrorHandler,
 	}
 	if err := app.Run(os.Args); err != nil {
 		fmt.Printf("Error: %v.", err)
