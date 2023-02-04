674dff855e5a0bf38cf7ec6aa1ff8d2e978cf0bf
jieggii <jieggii@pm.me>
2023-02-04 22:31:44 +0300

    Add `SIGINT` handler, change exit codes

--- cmd/dcfg/dcfg.go
+++ cmd/dcfg/dcfg.go
@@ -4,11 +4,25 @@ import (
 	"github.com/jieggii/dcfg/internal/app"
 	"github.com/jieggii/dcfg/internal/output"
 	"os"
+	"os/signal"
 )
 
+const interruptSignalExitCode = 130
+const genericErrorExitCode = 1
+
 func main() {
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt)
+	go func() {
+		for range signalChannel {
+			output.Stdout.Println()
+			output.Error.Println("interrupt signal received")
+			os.Exit(interruptSignalExitCode)
+		}
+	}()
+
 	if err := app.NewApp().Run(os.Args); err != nil {
-		output.Error.Printf("%v.\n", err)
-		os.Exit(1)
+		output.Error.Println(err)
+		os.Exit(genericErrorExitCode)
 	}
 }
