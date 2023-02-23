85cc69d258fff2a3ae04102ef991571ccd6ae953
jieggii <jieggii@pm.me>
2023-02-23 10:37:10 +0200

    Small fix for confirmation prompt

--- internal/input/confirmation.go
+++ internal/input/confirmation.go
@@ -11,12 +11,10 @@ func ConfirmationPrompt(prompt string) (bool, error) {
 	fmt.Printf("%v [Y/n] ", prompt)
 	reader := bufio.NewReader(os.Stdin)
 	choice, _ := reader.ReadString('\n')
-	choice = strings.ToLower(choice)
 	choice = strings.TrimRight(choice, "\n")
-	if choice == "" || choice == "y" {
+	if choice == "" || choice == "y" || choice == "Y" {
 		return true, nil
 	} else {
 		return false, nil
 	}
-
 }
